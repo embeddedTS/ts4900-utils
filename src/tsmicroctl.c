1ead07b814c1e9b937ca1e0fbf7ed98c7e291e50
Mark Featherston <mark@embeddedarm.com>
2016-05-31 16:06:02 -0700

    Added support for reading silabs rev Added sleep mode

--- src/tsmicroctl.c
+++ src/tsmicroctl.c
@@ -46,19 +46,6 @@ int silabs_init()
 	return fd;
 }
 
-uint16_t* sread(int twifd, uint16_t *data)
-{
-	uint8_t tmp[30];
-	bzero(tmp, 30);
-	int i;
-
-    read(twifd, tmp, 30);
-    for (i = 0; i < 15; i++)
-    	data[i] = (tmp[i*2] << 8) | tmp[(i*2)+1];
-
-	return data;
-}
-
 // Scale voltage to silabs 0-2.5V
 uint16_t inline sscale(uint16_t data){
 	return data * 2.5 * 1024/1000;
@@ -71,13 +58,34 @@ uint16_t inline rscale(uint16_t data, uint16_t r1, uint16_t r2)
 	return sscale(ret);
 }
 
-#ifdef CTL
+void do_sleep(int twifd, int seconds)
+{
+	unsigned char dat[4] = {0};
+	int opt_sleepmode = 1; // Legacy mode on new boards
+	int opt_resetswitchwkup = 1;
+
+	dat[0]=(0x1 | (opt_resetswitchwkup << 1) |
+	  ((opt_sleepmode-1) << 4) | 1 << 6);
+	dat[3] = (seconds & 0xff);
+	dat[2] = ((seconds >> 8) & 0xff);
+	dat[1] = ((seconds >> 16) & 0xff);
+	write(twifd, &dat, 4);
+}
 
 void do_info(int twifd)
 {
-	uint16_t data[20];
-	memset(data, 0, sizeof(uint16_t));
-	sread(twifd, data);
+	uint16_t data[16];
+	uint8_t tmp[32];
+	int i, ret;
+
+	ret = read(twifd, tmp, 32);
+
+	if(ret != 32){
+		printf("I2C Read failed with %d\n", ret);
+		return;
+	}
+    for (i = 0; i <= 15; i++)
+    	data[i] = (tmp[i*2] << 8) | tmp[(i*2)+1];
 
 	if(strstr(model, "7970")) {
 		printf("VDD_ARM_CAP=%d\n", sscale(data[0]));
@@ -94,16 +102,20 @@ void do_info(int twifd)
 		printf("V1P8=%d\n", sscale(data[11]));
 		printf("V1P2=%d\n", sscale(data[12]));
 		printf("RAM_VREF=%d\n", sscale(data[13]));
-		printf("V3P3=%d\n", sscale(data[14]));
+		printf("V3P3=%d\n", rscale(data[14], 499, 499));
+		printf("SILABREV=%d\n", data[15]);
 	}
 }
+
+#ifdef CTL
 
 static void usage(char **argv) {
 	fprintf(stderr, "Usage: %s [OPTION] ...\n"
 	  "Technologic Systems Microcontroller Access\n"
 	  "\n"
 	  "  -h, --help            This message\n"
-	  "  -i, --info            Read all Silabs ADC values\n"
+	  "  -i, --info            Read all Silabs ADC values and rev\n"
+	  "  -s, --sleep <seconds> Put the board in a sleep mode for n seconds\n"
 	  "    All values are returned in mV except the P10 which are raw 12-bit values.\n\n",
 	  argv[0]
 	);
@@ -116,6 +128,7 @@ int main(int argc, char **argv)
 
 	static struct option long_options[] = {
 	  { "info", 0, 0, 'i' },
+	  { "sleep", 1, 0, 's' },
 	  { "help", 0, 0, 'h' },
 	  { 0, 0, 0, 0 }
 	};
@@ -135,10 +148,13 @@ int main(int argc, char **argv)
 	if(twifd == -1)
 		return 1;
 
-	while((c = getopt_long(argc, argv, "ih", long_options, NULL)) != -1) {
+	while((c = getopt_long(argc, argv, "is:h", long_options, NULL)) != -1) {
 		switch (c) {
 		case 'i':
 			do_info(twifd);
+			break;
+		case 's':
+			do_sleep(twifd, atoi(optarg));
 			break;
 		case 'h':
 		default:
