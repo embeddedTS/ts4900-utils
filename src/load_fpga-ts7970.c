39c18f948358e7fa6c2a97229af529779d4b0044
Mark Featherston <mark@embeddedarm.com>
2017-06-27 11:18:48 -0700

    Fixed the TS-7970 fpga reloading setup for updated ispvm.c

--- src/load_fpga-ts7970.c
+++ src/load_fpga-ts7970.c
@@ -4,46 +4,66 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+
 #include "gpiolib-fast.h"
+#include "ispvm.h"
 
 volatile uint32_t *mx6gpio;
 static unsigned int gpiostate;
 
-static const unsigned int g_ucPinTDI     = 1<<16;   /* Bit address of TDI */
-static const unsigned int g_ucPinTCK     = 1<<11;   /* Bit address of TCK */
-static const unsigned int g_ucPinTMS     = 1<<8;   /* Bit address of TMS */
-static const unsigned int g_ucPinTDO     = 1<<12;   /* Bit address of TDO*/
+#define TDI 1<<16
+#define TCK 1<<11
+#define TMS 1<<8
+#define TDO 1<<12
 
 void init_ts7970(void)
 {
 	mx6gpio = gpiofast_init();
 	assert(mx6gpio != 0);
-	mx6gpio[(MX6_GPIO_BANK5 + GPGDIR)/4] |= g_ucPinTDI | g_ucPinTCK | g_ucPinTMS;
-	mx6gpio[(MX6_GPIO_BANK5 + GPGDIR)/4] &= ~g_ucPinTDO;
+
+	mx6gpio[(MX6_GPIO_BANK5 + GPGDIR)/4] |= TDI  | TMS | TCK;
+	mx6gpio[(MX6_GPIO_BANK5 + GPGDIR)/4] &= ~(TDO);
+
+	mx6gpio[(MX6_GPIO_BANK5 + GPDR)/4] &= ~(TCK);
 	gpiostate = mx6gpio[(MX6_GPIO_BANK5 + GPDR)/4];
 }
 
 void restore_ts7970(void)
 {
-	mx6gpio[(MX6_GPIO_BANK5 + GPGDIR)/4] &= ~(g_ucPinTDI | g_ucPinTCK | g_ucPinTMS);
+	mx6gpio[(MX6_GPIO_BANK5 + GPGDIR)/4] &= ~(TDI | TCK | TMS);
 }
 
 int readport_ts7970(void)
 {
-	return (mx6gpio[(MX6_GPIO_BANK5 + GPPSR)/4] & g_ucPinTDO) ? 1 : 0;
+	return (mx6gpio[(MX6_GPIO_BANK5 + GPPSR)/4] & TDO) ? 1 : 0;
 }
 
 void writeport_ts7970(int pins, int val)
 {
-	if (val) gpiostate |= pins;
-	else gpiostate &= ~pins;
+	gpiostate = mx6gpio[(MX6_GPIO_BANK5 + GPDR)/4];
+
+	if(val) {
+		if(pins & g_ucPinTDI)
+			gpiostate |= TDI;
+		if(pins & g_ucPinTCK)
+			gpiostate |= TCK;
+		if(pins & g_ucPinTMS)
+			gpiostate |= TMS;
+	} else {
+		if(pins & g_ucPinTDI)
+			gpiostate &= ~TDI;
+		if(pins & g_ucPinTCK)
+			gpiostate &= ~TCK;
+		if(pins & g_ucPinTMS)
+			gpiostate &= ~TMS;
+	}
 
 	mx6gpio[(MX6_GPIO_BANK5 + GPDR)/4] = gpiostate;
 }
 
 void sclock_ts7970()
 {
-	assert((gpiostate & g_ucPinTCK) == 0);
-	mx6gpio[(MX6_GPIO_BANK5 + GPDR)/4] = g_ucPinTCK;
-	mx6gpio[(MX6_GPIO_BANK5 + GPDR)/4] = ~(g_ucPinTCK);
+	assert((gpiostate & TCK) == 0);
+	mx6gpio[(MX6_GPIO_BANK5 + GPDR)/4] |= TCK;
+	mx6gpio[(MX6_GPIO_BANK5 + GPDR)/4] &= ~(TCK);
 }
