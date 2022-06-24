0f872d04d454ba8d4dea1946734eb8744caa79cf
Mark Featherston <mark@embeddedts.com>
2022-06-24 15:35:52 -0700

    fpga: Updated layer to use i2c ioctl to allow concurrent access with gpio

--- src/fpga.c
+++ src/fpga.c
@@ -6,8 +6,11 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include "i2c-dev.h"
+
+#define FPGA_ADDR 0x28
 
 int fpga_init()
 {
@@ -41,8 +44,8 @@ int fpga_init()
 	}
 
 	if(fd != -1) {
-		if (ioctl(fd, I2C_SLAVE_FORCE, 0x28) < 0) {
-			perror("FPGA did not ACK 0x28\n");
+		if (ioctl(fd, I2C_SLAVE_FORCE, FPGA_ADDR) < 0) {
+			perror("FPGA did not ACK\n");
 			return -1;
 		}
 	}
@@ -50,26 +53,87 @@ int fpga_init()
 	return fd;
 }
 
-void fpoke8(int twifd, uint16_t addr, uint8_t value)
+
+int fpeekstream8(int twifd, uint8_t *data, uint16_t addr, int size)
 {
-	uint8_t data[3];
-	data[0] = ((addr >> 8) & 0xff);
-	data[1] = (addr & 0xff);
-	data[2] = value;
-	if (write(twifd, data, 3) != 3) {
-		perror("I2C Write Failed");
+	struct i2c_rdwr_ioctl_data packets;
+	struct i2c_msg msgs[2];
+	char busaddr[2];
+
+	/* Linux only supports 4k transactions at a time, and we need
+	 * two bytes for the address */
+	assert(size <= 4094);
+
+	busaddr[0]    = ((addr >> 8) & 0xff);
+	busaddr[1]    = (addr & 0xff);
+
+	msgs[0].addr  = FPGA_ADDR;
+	msgs[0].flags = 0;
+	msgs[0].len   = 2;
+	msgs[0].buf   = busaddr;
+
+	msgs[1].addr  = FPGA_ADDR;
+	msgs[1].flags = I2C_M_RD;
+	msgs[1].len   = size;
+	msgs[1].buf   = (char *)data;
+
+	packets.msgs = msgs;
+	packets.nmsgs = 2;
+
+	if(ioctl(twifd, I2C_RDWR, &packets) < 0) {
+		perror("Unable to read I2C data");
+		return 1;
+	}
+	return 0;
+}
+
+int fpokestream8(int twifd, uint8_t *data, uint16_t addr, int size)
+{
+	struct i2c_rdwr_ioctl_data packets;
+	struct i2c_msg msg;
+	uint8_t outdata[4096];
+
+	/* Linux only supports 4k transactions at a time, and we need
+	 * two bytes for the address */
+	assert(size <= 4094);
+
+	outdata[0] = ((addr >> 8) & 0xff);
+	outdata[1] = (addr & 0xff);
+	memcpy(&outdata[2], data, size);
+
+	msg.addr   = FPGA_ADDR;
+	msg.flags  = 0;
+	msg.len	   = 2 + size;
+	msg.buf	   = (char *)outdata;
+
+	packets.msgs = &msg;
+	packets.nmsgs = 1;
+
+	if(ioctl(twifd, I2C_RDWR, &packets) < 0) {
+		perror("Unable to send I2C data");
+		return 1;
+	}
+	return 0;
+}
+
+void fpoke8(int twifd, uint16_t addr, uint8_t data)
+{
+	int ret;
+
+	ret = fpokestream8(twifd, &data, addr, 1);
+	if (ret) {
+		perror("Failed to write to FPGA");
 	}
 }
 
 uint8_t fpeek8(int twifd, uint16_t addr)
 {
-	uint8_t data[2];
-	data[0] = ((addr >> 8) & 0xff);
-	data[1] = (addr & 0xff);
-	if (write(twifd, data, 2) != 2) {
-		perror("I2C Address set Failed");
-	}
-	read(twifd, data, 1);
+	uint8_t data = 0;
+	int ret;
+	ret = fpeekstream8(twifd, &data, addr, 1);
 
-	return data[0];
+	if (ret) {
+		perror("Failed to read from FPGA");
+	}
+	return data;
 }
