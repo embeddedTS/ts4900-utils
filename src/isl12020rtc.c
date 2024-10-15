136926f333c532a60dcdfdded0f7846b4adbd487
Kris Bahnsen <kris@embeddedTS.com>
2024-10-15 14:36:04 -0700

    isl12020rtc: Clean up error handling, match other implementations
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- src/isl12020rtc.c
+++ src/isl12020rtc.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdint.h>
@@ -10,7 +11,8 @@
 #include <time.h>
 #include <assert.h>
 
-#include "i2c-dev.h"
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
 
 #define ISL12022_REG_OFF_VAL 0x21
 #define ISL12022_REG_OFF_CTL 0x25
@@ -18,7 +20,7 @@
 #define ISL12022_OFF_CTL_ADD (1 << 1) /* 1 if the value is add, 0 if subtract */
 #define ISL12022_OFF_CTL_FLASH (1 << 2) /* 1 to commit to flash, 0 to just ram */
 
-int rtc_init()
+int rtc_init(void)
 {
 	static int fd = -1;
 	DIR *d;
@@ -32,97 +34,116 @@ int rtc_init()
 	// search for the name 21a0000.i2c where the name is the
 	// memory address in the imx6 of the correct i2c bus.
 	d = opendir("/sys/bus/i2c/devices/");
-	if (d) {
-		while ((dir = readdir(d)) != NULL) {
-			char path[512], busname[512];
-			int namefd;
-			snprintf(path, 512, "/sys/bus/i2c/devices/%s/name", dir->d_name);
-			namefd = open(path, O_RDONLY);
-			if (namefd == -1)
-				continue;
-			if (read(namefd, busname, 512) == -1)
-				perror("busname");
-			if (strncmp(busname, "21a0000.i2c", 11) == 0) {
-				snprintf(path, 512, "/dev/%s", dir->d_name);
-				fd = open(path, O_RDWR);
-			}
-		}
-		closedir(d);
+	if (d == NULL) {
+		perror("Unable to open i2c sys directory");
+		return -1;
 	}
 
-	if (fd != -1) {
-		if (ioctl(fd, I2C_SLAVE_FORCE, 0x6f) < 0) {
-			perror("FPGA did not ACK 0x6f\n");
-			return -1;
+	while ((dir = readdir(d)) != NULL) {
+		char path[512], busname[512];
+		int namefd;
+		snprintf(path, 512, "/sys/bus/i2c/devices/%s/name", dir->d_name);
+		namefd = open(path, O_RDONLY);
+		if (namefd == -1)
+			continue;
+		if (read(namefd, busname, 512) == -1) {
+			perror("Unable to read from bus name file");
+			fd = -1;
+			break;
+		}
+		if (strncmp(busname, "21a0000.i2c", 11) == 0) {
+			snprintf(path, 512, "/dev/%s", dir->d_name);
+			fd = open(path, O_RDWR);
+			if (fd < 0) {
+				perror("Unable to open RTC");
+				break;
+			}
 		}
 	}
+	closedir(d);
 
 	return fd;
 }
 
-void rtc_read(int i2cfd, uint8_t addr, void *data, uint8_t len)
+int rtc_read(int i2cfd, uint8_t addr, void *data, uint8_t len)
 {
 	struct i2c_rdwr_ioctl_data packets;
 	struct i2c_msg msgs[2];
-	int retry = 0;
+	int ret;
 
-retry:
 	msgs[0].addr = 0x6f;
 	msgs[0].flags = 0;
 	msgs[0].len = 1;
-	msgs[0].buf = (char *)&addr;
+	msgs[0].buf = &addr;
 
 	msgs[1].addr = 0x6f;
 	msgs[1].flags = I2C_M_RD;
 	msgs[1].len = len;
-	msgs[1].buf = (char *)data;
+	msgs[1].buf = (uint8_t *)data;
 
 	packets.msgs = msgs;
 	packets.nmsgs = 2;
 
-	if (ioctl(i2cfd, I2C_RDWR, &packets) < 0) {
+	/* I2C_RDWR will return < 0 on error, or the number of messages that
+	 * were transferred. We should always have two since we are only ever
+	 * sending a write followed by a read.
+	 */
+	ret = ioctl(i2cfd, I2C_RDWR, &packets);
+	if (ret < 0)
 		perror("Unable to read data");
-		retry++;
-		if (retry < 10)
-			goto retry;
-	}
+	else if (ret == 2)
+		ret = 0;
+	else
+		ret = -1;
+
+	return ret;
 }
 
-void rtc_write(int i2cfd, uint8_t addr, uint8_t data)
+int rtc_write(int i2cfd, uint8_t addr, uint8_t data)
 {
 	struct i2c_rdwr_ioctl_data packets;
 	struct i2c_msg msg;
-	int retry = 0;
+	int ret;
 	uint8_t tmp[2];
 
 	tmp[0] = addr;
 	tmp[1] = data;
 
-retry:
 	msg.addr = 0x6f;
 	msg.flags = 0;
 	msg.len = 2;
-	msg.buf = (char *)tmp;
+	msg.buf = tmp;
 
 	packets.msgs = &msg;
 	packets.nmsgs = 1;
 
-	if (ioctl(i2cfd, I2C_RDWR, &packets) < 0) {
-		perror("Unable to write data");
-		retry++;
-		if (retry < 10)
-			goto retry;
-	}
+	/* I2C_RDWR will return < 0 on error, or the number of messages that
+	 * were transferred. We should always have one since we are only ever
+         * sending a single transfer.
+	 */
+	ret = ioctl(i2cfd, I2C_RDWR, &packets);
+	if (ret < 0)
+		perror("Unable to read data");
+	else if (ret == 1)
+		ret = 0;
+	else
+		ret = -1;
+
+	return ret;
 }
 
 /* Return temp n millicelcius */
-int rtc_temp_read(int i2cfd)
+int rtc_temp_read(int i2cfd, int *mc)
 {
 	uint8_t data[2];
-	rtc_read(i2cfd, 0x28, data, 2);
 
-	/* Convert from Kelvin */
-	return ((data[0] | (data[1] << 8)) * 500) - 273000;
+	if (rtc_read(i2cfd, 0x28, data, 2) < 0)
+		return -1;
+
+	/* Convert from Kelvin to millicelsius */
+	*mc = ((data[0] | (data[1] << 8)) * 500) - 273000;
+
+	return 0;
 }
 
 int bcd_to_decimal(uint8_t bcd)
@@ -132,12 +153,13 @@ int bcd_to_decimal(uint8_t bcd)
 	return dec;
 }
 
-void rtc_tsv2b_read(int i2cfd, struct tm *ts)
+int rtc_tsv2b_read(int i2cfd, struct tm *ts)
 {
 	time_t now;
 	uint8_t data[5];
 
-	rtc_read(i2cfd, 0x16, data, 5);
+	if (rtc_read(i2cfd, 0x16, data, 5) < 0)
+		return -1;
 	time(&now);
 	gmtime_r(&now, ts);
 
@@ -147,14 +169,17 @@ void rtc_tsv2b_read(int i2cfd, struct tm *ts)
 	ts->tm_mday = bcd_to_decimal(data[3] & 0x3f);
 	ts->tm_mon = bcd_to_decimal(data[4] & 0x1f) - 1; /* hardware is 1-12, remove 1 to match 0 numbering*/
 	/* Year will always match current year! */
+
+	return 0;
 }
 
-void rtc_tsb2v_read(int i2cfd, struct tm *ts)
+int rtc_tsb2v_read(int i2cfd, struct tm *ts)
 {
 	time_t now;
 	uint8_t data[5];
 
-	rtc_read(i2cfd, 0x1b, data, 5);
+	if (rtc_read(i2cfd, 0x1b, data, 5) < 0)
+		return -1;
 	time(&now);
 	gmtime_r(&now, ts);
 
@@ -164,58 +189,84 @@ void rtc_tsb2v_read(int i2cfd, struct tm *ts)
 	ts->tm_mday = bcd_to_decimal(data[3] & 0x3f);
 	ts->tm_mon = bcd_to_decimal(data[4] & 0x1f) - 1; /* hardware is 1-12, remove 1 to match 0 numbering*/
 	/* Year will always match current year! */
+
+	return 0;
 }
 
-void rtc_clear_time_stamp(int i2cfd)
+int rtc_clear_time_stamp(int i2cfd)
 {
 	uint8_t data;
 
-	rtc_read(i2cfd, 0x09, &data, 1);
+	if (rtc_read(i2cfd, 0x09, &data, 1) < 0) {
+		fprintf(stderr, "Unable to read timestamp\n");
+		return -1;
+	}
+
 	data |= (1 << 7); /* CLRTS */
-	rtc_write(i2cfd, 0x09, data);
+	if (rtc_write(i2cfd, 0x09, data) < 0) {
+		fprintf(stderr, "Unable to clear timestamp\n");
+		return -1;
+	}
+
+	return 0;
 }
 
-int rtc_is_emulated(int i2cfd)
+int rtc_is_emulated(int i2cfd, int *emulated)
 {
 	uint8_t data;
 
-	rtc_read(i2cfd, 0x0f, &data, 1);
-	return !!(data & (1 << 7));
+	if (rtc_read(i2cfd, 0x0f, &data, 1) < 0)
+		return -1;
+
+	*emulated = !!(data & (1 << 7));
+	return 0;
 }
 
-void rtc_offset_set(int i2cfd, long offset)
+int rtc_offset_set(int i2cfd, long offset)
 {
 	uint8_t data;
 	uint32_t ppb = labs(offset);
+	int i;
 
-	data = (uint8_t)(ppb >> 0);
-	rtc_write(i2cfd, ISL12022_REG_OFF_VAL + 0, data);
-	data = (uint8_t)(ppb >> 8);
-	rtc_write(i2cfd, ISL12022_REG_OFF_VAL + 1, data);
-	data = (uint8_t)(ppb >> 16);
-	rtc_write(i2cfd, ISL12022_REG_OFF_VAL + 2, data);
-	data = (uint8_t)(ppb >> 24);
-	rtc_write(i2cfd, ISL12022_REG_OFF_VAL + 3, data);
+	/* Write the ppb to the associated offset registers. This is written
+	 * LSB first.
+	 *
+	 * TODO: Rather than looping on a byte write, it would likely be possible
+	 * to modify the rtc_write() function to accept a number of bytes to
+	 * transmit.
+	 */
+	for (i = 0; i < 4; i++) {
+		data = (uint8_t)((ppb >> (i*8)) & 0xff);
+		if (rtc_write(i2cfd, ISL12022_REG_OFF_VAL + i, data) < 0)
+			return -1;
+	}
 
-	data = ISL12022_OFF_CTL_APPLY | ((offset > 0) ? ISL12022_OFF_CTL_ADD : 0) | ISL12022_OFF_CTL_FLASH;
-	rtc_write(i2cfd, ISL12022_REG_OFF_CTL, data);
+	data = ISL12022_OFF_CTL_APPLY |
+	       ((offset > 0) ? ISL12022_OFF_CTL_ADD : 0) |
+	       ISL12022_OFF_CTL_FLASH;
+
+	if (rtc_write(i2cfd, ISL12022_REG_OFF_CTL, data) < 0)
+			return -1;
+
+	return 0;
 }
 
-long rtc_offset_get(int i2cfd)
+int rtc_offset_get(int i2cfd, long *offset)
 {
 	uint8_t data;
 	uint32_t ppb;
-	long offset;
 
-	rtc_read(i2cfd, ISL12022_REG_OFF_VAL, &ppb, (sizeof(ppb)));
-	rtc_read(i2cfd, ISL12022_REG_OFF_CTL, &data, 1);
+	if (rtc_read(i2cfd, ISL12022_REG_OFF_VAL, &ppb, (sizeof(ppb))) < 0)
+		return -1;
+	if (rtc_read(i2cfd, ISL12022_REG_OFF_CTL, &data, 1) < 0)
+		return -1;
 
-	offset = ppb;
+	*offset = ppb;
 
 	if ((data & ISL12022_OFF_CTL_ADD) == 0)
-		offset *= -1;
+		*offset *= -1;
 
-	return offset;
+	return 0;
 }
 
 int main(int argc, char **argv)
@@ -224,6 +275,9 @@ int main(int argc, char **argv)
 	int temp;
 	struct tm tsv2b, tsb2v;
 	char tmbuf[64];
+	long offset;
+	int emulated;
+	int ret = 1;
 
 	i2cfd = rtc_init();
 	if (i2cfd == -1)
@@ -234,25 +288,37 @@ int main(int argc, char **argv)
 		float ppm = atof(argv[1]);
 		long ppb = (long)(ppm * 1000 * -1);
 
-		rtc_offset_set(i2cfd, ppb);
+		if (rtc_offset_set(i2cfd, ppb) < 0)
+			goto out;
 	}
 
-	temp = rtc_temp_read(i2cfd);
+	if (rtc_temp_read(i2cfd, &temp) < 0)
+		goto out;
 	printf("rtctemp_millicelcius=%d\n", temp);
 
-	rtc_tsv2b_read(i2cfd, &tsv2b);
+	if (rtc_tsv2b_read(i2cfd, &tsv2b) < 0)
+		goto out;
 	strftime(tmbuf, 64, "%m-%d %H:%M:%S", &tsv2b);
 	printf("poweroff_utc_timestamp_tsv2b=\"%s\"\n", tmbuf);
 
-	rtc_tsb2v_read(i2cfd, &tsb2v);
+	if (rtc_tsb2v_read(i2cfd, &tsb2v) < 0)
+		goto out;
 	strftime(tmbuf, 64, "%m-%d %H:%M:%S", &tsb2v);
 	printf("poweron_utc_timestamp_tsb2v=\"%s\"\n", tmbuf);
 
-	printf("rtc_is_emulated=%d\n", rtc_is_emulated(i2cfd));
-	printf("offset_ppb=%ld\n", rtc_offset_get(i2cfd));
+	if (rtc_is_emulated(i2cfd, &emulated) < 0)
+		goto out;
+	printf("rtc_is_emulated=%d\n", emulated);
 
-	rtc_clear_time_stamp(i2cfd);
+	if (rtc_offset_get(i2cfd, &offset) < 0)
+		goto out;
+	printf("offset_ppb=%ld\n", offset);
 
+	if (rtc_clear_time_stamp(i2cfd) < 0)
+		goto out;
+
+	ret = 0;
+out:
 	close(i2cfd);
-	return 0;
+	return ret;
 }
